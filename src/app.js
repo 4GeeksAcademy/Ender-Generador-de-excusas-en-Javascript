0a28d7d99cea2ee33f48245dd8fefbe194a50193
Ender Calderón <177250779+Yohebyth@users.noreply.github.com>
2024-11-17 22:23:29 +0000

    Lineamientos de Codigo

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
-/* eslint-disable */
 import "./style.css";
 import "./assets/img/4geeks.ico";
 
 window.onload = () => {
-  //write your code here
   let who = ["The dog", "My grandma", "The mailman", "My bird"];
   let action = ["ate", "peed", "crushed", "broke"];
   let what = ["my homework", "my phone", "the car"];
@@ -15,19 +13,17 @@ window.onload = () => {
     "while I was praying"
   ];
 
-  function ramdomElement(arr) {
+  const ramdomElement = arr => {
     return Math.floor(Math.random() * arr.length);
-  }
-  // Decloro el string imprimir, en el se guardan todas las partes de la excusa, se usa += para conservar el valor actual y agregar el valor nuevo
-  let Imprimir = "";
-  // En los pasos siguientes se usa un random para generar la posicion de cada arreglo segun la cantidad de lapabras que tenga y se guarda en imprimir
-  Imprimir =
-    who[ramdomElement(who)] +
-    " " +
-    action[ramdomElement(action)] +
-    " " +
-    what[ramdomElement(what)] +
-    " " +
-    when[ramdomElement(when)];
-  document.querySelector("#excuse").innerHTML = Imprimir; // cambia la palabra excuse en el HTML por la excusa generada
+  };
+
+  let imprimir = "";
+  imprimir = `
+    ${who[ramdomElement(who)]} 
+    ${action[ramdomElement(action)]} 
+    ${what[ramdomElement(what)]} 
+    ${when[ramdomElement(when)]}
+  `;
+
+  document.querySelector("#excuse").innerHTML = imprimir;
 };
